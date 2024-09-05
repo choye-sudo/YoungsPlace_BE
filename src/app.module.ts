db939718868bc3cd8196e26c88867bc31e7b7dc5
llcodingll <ll.o.llwhkr@gmail.com>
2024-09-05 16:44:02 +0900

    save & savelist 컨트롤러, 서비스, 모듈

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { ProvinceModule } from './province/province.module';
 import { CityModule } from './city/city.module';
 import { RecruitmentStatusModule } from './recruitment-status/recruitment-status.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { SaveModule } from './save/save.module';
 
 @Module({
-  imports: [ProvinceModule, CityModule, RecruitmentStatusModule, PrismaModule],
+  imports: [ProvinceModule, CityModule, RecruitmentStatusModule, PrismaModule, SaveModule],
   controllers: [AppController],
   providers: [AppService],
 })
